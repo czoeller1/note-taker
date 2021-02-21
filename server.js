8304cdfde5fa9017b9961840e68761e0a6c98f31
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-21 15:02:01 -0700

    added initial files

---
+++ server.js
@@ -0,0 +1,46 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// Routes
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+// Displays all characters
+app.get('/api/notes', (req, res) => res.json(path.join(__dirname, "/db/db.json")));
+
+
+
+// Create New Characters - takes in JSON input
+app.post('/api/notes', (req, res) => {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  const newCharacter = req.body;
+    
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
+  console.log(newCharacter);
+
+  characters.push(newCharacter);
+  res.json(newCharacter);
+});
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
