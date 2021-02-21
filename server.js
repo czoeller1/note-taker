b9e74762b83abf98d7ed0c977e9cd40dcfd002fa
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-21 15:27:18 -0700

    test

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // Basic route that sends the user first to the AJAX Page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.get('/', (req, res) => console.log(__dirname, req));
+app.get('/test', (req, res) => console.log(__dirname, req));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
