15039354bf8b1793eeae83e1acc832ef2994b37a
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-21 15:24:50 -0700

    log for heroku

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(express.json());
 // Basic route that sends the user first to the AJAX Page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
+app.get('/', (req, res) => console.log(__dirname, req));
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 // Displays all characters
@@ -31,7 +33,7 @@ app.post('/api/notes', (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   const newCharacter = req.body;
-    
+
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
